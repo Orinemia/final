c9af5bbfda01bcbfbf513758f83d5bb0f303edc5
Orinemia <o.ajulo.13@aberdeen.ac.uk>
2014-05-06 09:16:43 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+President::Application.config.secret_key_base = 'abf6d973d739e9980daedb68eb0060b22a049172dbc2da210341628bbc5eef3374a1ba5e864d7c4fd8207883086aa6bf30c736cd63049236cf304c3ac01cd4de'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+President::Application.config.session_store :cookie_store, key: '_president_session'
