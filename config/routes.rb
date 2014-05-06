a33b99a985f92fa8677bd98d0a4c4ce1fdce5b50
Orinemia <o.ajulo.13@aberdeen.ac.uk>
2014-05-06 12:09:57 +0100

    Heroku testing

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ President::Application.routes.draw do
 
   resources :cics
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'cics#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
