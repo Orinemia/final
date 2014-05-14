4f36714a1d9588989111016fd693e888fc45b9d7
Orinemia <o.ajulo.13@aberdeen.ac.uk>
2014-05-14 20:19:55 +0100

    start again

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,6 @@ module President
     
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-    config.serve_static_assets = false
+    # config.i18n.default_locale = :
   end
 end
