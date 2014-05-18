565a11fb348543dc6e9c881b14980977925c3330
Orinemia <o.ajulo.13@aberdeen.ac.uk>
2014-05-18 22:36:14 +0100

    fava

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ President::Application.routes.draw do
   resources :cics
   resources :sessions, only: [:new, :create, :destroy]
  
-  match '/signin',    to: 'sessions#new',          via: 'get'
-  match '/signout',   to: 'sessions#destroy',      via: 'delete'
+  #match '/signin',    to: 'sessions#new',          via: 'get'
+  #match '/signout',   to: 'sessions#destroy',      via: 'delete'
 
   match '/signup',    to: 'users#new',             via: 'get'
   match '/contactus', to: 'navigation#contact',    via: 'get'   
