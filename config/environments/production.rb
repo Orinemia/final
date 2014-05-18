60756d4ddc7505597b55097c031dabb6543d25ff
Orinemia <o.ajulo.13@aberdeen.ac.uk>
2014-05-18 22:21:34 +0100

    aaa

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-FinanceApp::Application.configure do
+President::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
